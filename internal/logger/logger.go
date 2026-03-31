cf26d1d393b37e7c70d02a74ac7c34708be9e7f5
nikallow <nikallowdev@gmail.com>
2026-03-31 09:34:43 +0300

    feat(logger): add slog logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,47 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/nikallow/auth-service/internal/config"
+)
+
+func New(cfg *config.Config) *slog.Logger {
+	opts := &slog.HandlerOptions{
+		Level:     parseLevel(cfg.Logger.Level),
+		AddSource: cfg.Env == "local",
+	}
+
+	var handler slog.Handler
+
+	switch strings.ToLower(cfg.Logger.Format) {
+	case "text":
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	case "json":
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	default:
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	}
+
+	return slog.New(handler).With(
+		slog.String("service", cfg.Service.Name),
+		slog.String("env", cfg.Env),
+	)
+}
+
+func parseLevel(level string) slog.Level {
+	switch strings.ToLower(level) {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
